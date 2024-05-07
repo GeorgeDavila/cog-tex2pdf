7cdf50c057da212f1d92bf15ea9690d625136b91
George Davila Durendal <gaddog94@gmail.com>
2024-05-07 16:13:12 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,21 +4,19 @@
 from cog import BasePredictor, Input, Path
 from pylatex import Document, Section, Subsection, Command
 from pylatex.utils import italic, NoEscape
+from pylatex.section import Chapter, Paragraph, Subparagraph
 
-def fill_document(doc, text2type):
+def fill_document(doc, chapterTitleString, text2type):
     """Add a section, a subsection and some text to the document.
 
     :param doc: the document
     :type doc: :class:`pylatex.document.Document` instance
     """
     text2type = str(text2type)
+    chapterTitle = str(chapterTitleString)
     
-    with doc.create(Section('A section')):
+    with doc.create(Section(chapterTitle)):
         doc.append(text2type)
-        #doc.append(italic(text2type))
-
-        #with doc.create(Subsection('A subsection')):
-        #    doc.append('Also some crazy characters: $&#{}')
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
@@ -26,6 +24,10 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
+        chapterTitleInput: str = Input(
+            description="Title", 
+            default="Report",
+            ),
         textInput: str = Input(
             description="Text to enter into the document.", 
             default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -38,7 +40,7 @@ class Predictor(BasePredictor):
 
         documentName = "generated"
         doc = Document(documentName)
-        fill_document(doc, textInput)
+        fill_document(doc, chapterTitleInput, textInput)
 
         doc.generate_pdf(clean_tex=False)
         doc.generate_tex()
